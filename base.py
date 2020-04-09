76c6f4724299a453e1f719d5c71fe7125f008711
Parmeet Singh <58912098+ParmeetAFK@users.noreply.github.com>
2020-04-10 00:59:45 +0530

    Comments and Updated Functionality

--- base.py
+++ base.py
@@ -9,14 +9,24 @@ soup2 = BeautifulSoup(col_page)
 for i in soup2.findAll('a' , attrs={'class' : ("text--bold palewhite title") }):
 	movielinks.append(i)
 
-print(type(movielinks[0].href))
-print(movielinks[0])
+boss = []
+
+
+
 
 def get_files():
 
+
+	# ----------------------------  Gets Download Links  --------------------------------------------- #
+
+	#================================  URL without end variable  ========================================#
+
+	#================================  List index values  ========================================#
+
+
 	movie_page = urlopen("https://yts.ms/movie/underwater-2020")
 	soup = BeautifulSoup(movie_page)
-	links = []
+	links = [] 
 	popu = []
 
 	for link in soup.findAll('a', attrs={'href': re.compile("^https://yts.lt/torrent/download/")}):
@@ -62,10 +72,17 @@ def get_files():
 
 
 def download_file():
+
+	# ----------------------------  Downloads Torrent Files  --------------------------------------------- #
+
+	#================================  URL without end variable  ========================================#
+
 	XXSurl = 'https://yts.mx/torrent/download/7A35102A1B717B98F73975C492ABB7A23BBE548D'
 	r = requests.get(url, allow_redirects=True)
 
 	open('bless.torrent', 'wb').write(r.content)
+
+
 
 
 #['https://yts.lt/torrent/download/8BF2C6472B247569739A5DCA17BBFC0457F94B9A', 'https://yts.lt/torrent/download/DFAEA19933AB21A461848516B31050C8F6BFF0DD', 'https://yts.lt/torrent/download/8BF2C6472B247569739A5DCA17BBFC0457F94B9A', 'https://yts.lt/torrent/download/DFAEA19933AB21A461848516B31050C8F6BFF0DD']
